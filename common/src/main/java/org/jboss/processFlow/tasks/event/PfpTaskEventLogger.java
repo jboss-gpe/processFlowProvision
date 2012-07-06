96c71b60086c91688e9fa99ac85136408856bf61
Jeffrey Bride <jbride2001@yahoo.com>
2012-07-06 13:42:43 -0400

    new module descriptor files

--- common/src/main/java/org/jboss/processFlow/tasks/event/PfpTaskEventLogger.java
+++ common/src/main/java/org/jboss/processFlow/tasks/event/PfpTaskEventLogger.java
@@ -15,52 +15,42 @@ public class PfpTaskEventLogger implements PfpTaskEventListener {
 
     private static final Logger logger = LoggerFactory.getLogger(PfpTaskEventLogger.class);
 
-    @Override
     public void taskClaimed(TaskClaimedEvent event) {
         logger.info("task {} claimed by {}", event.getTaskId(), event.getUserId());
     }
 
-    @Override
     public void taskCompleted(TaskCompletedEvent event) {
         logger.info("task {} completed by {}", event.getTaskId(), event.getUserId());
     }
 
-    @Override
     public void taskFailed(TaskFailedEvent event) {
         logger.info("task {} failed by {}", event.getTaskId(), event.getUserId());
     }
 
-    @Override
     public void taskSkipped(TaskSkippedEvent event) {
         logger.info("task {} skipped by {}", event.getTaskId(), event.getUserId());
     }
 
-    @Override
     public void taskDelegated(TaskDelegatedEvent event) {
         logger.info("task {} delegated by {}", event.getTaskId(), event.getUserId());
     }
 
-    @Override
     public void taskCreated(TaskCreatedEvent event) {
         logger.info("task {} created", event.getTaskId());
     }
 
-    @Override
     public void taskReleased(TaskReleasedEvent event) {
         logger.info("task {} released by {}", event.getTaskId(), event.getUserId());
     }
 
-    @Override
     public void taskStarted(TaskStartedEvent event) {
         logger.info("task {} started by {}", event.getTaskId(), event.getUserId());
     }
 
-    @Override
     public void taskForwarded(TaskForwardedEvent event) {
         logger.info("task {} forwarded by {}", event.getTaskId(), event.getUserId());
     }
 
-    @Override
     public void taskStopped(TaskStoppedEvent event) {
         logger.info("task {} stopped by {}", event.getTaskId(), event.getUserId());
     }
