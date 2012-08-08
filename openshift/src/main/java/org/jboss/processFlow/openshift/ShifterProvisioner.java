a0c38744b9a6ccec51915d0a071d060922e7389d
Jeffrey Bride <jbride2001@yahoo.com>
2012-08-08 00:00:18 -0400

    now validating openshift account details

--- openshift/src/main/java/org/jboss/processFlow/openshift/ShifterProvisioner.java
+++ openshift/src/main/java/org/jboss/processFlow/openshift/ShifterProvisioner.java
@@ -1,21 +1,62 @@
 package org.jboss.processFlow.openshift;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.InputStream;
+import java.io.IOException;
 import java.util.Properties;
+
+import javax.xml.XMLConstants;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+
+import org.xml.sax.SAXException;
+
 import org.apache.log4j.Logger;
 
 public class ShifterProvisioner {
 
-	public static final String OPENSHIFT_PFP_PROPERTIES = "/openshift.pfp.properties";
+    public static final String OPENSHIFT_PFP_PROPERTIES = "/openshift.pfp.properties";
     public static final String OPENSHIFT_HOST = "openshift.host";
     public static final String OPENSHIFT_ACCOUNT_DETAILS_FILE_LOCATION  = "openshift.account.details.file.location";
     public static final String OPENSHIFT_ACCOUNT_PROVISIONING_LOG_DIR  = "openshift.account.provisioning.log.dir";
+    public static final String OPENSHIFT_ACCOUNT_DETAILS_SCHEMA_FILE="/openshift_account_details.xsd";
 
     private static Logger log = Logger.getLogger("ShifterProvisioner"); 
     private static String openshiftHost;
+    private static String openshiftAccountDetailsFile;
+    private static String openshiftAccountProvisioningLogDir;
 
-    public static void main(String args[] ) {
+    public static void main(String args[] ) throws Exception{
         setProps();
+        validateAccountDetails();
+    }
+    
+    public static void validateAccountDetails() throws Exception {
+        SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+        InputStream xsdStream = null;
+        InputStream xmlStream = null;
+        try {
+        	xsdStream = ShifterProvisioner.class.getResourceAsStream(OPENSHIFT_ACCOUNT_DETAILS_SCHEMA_FILE);
+        	if(xsdStream == null)
+        		throw new RuntimeException("validateAccountDetails() can't find schema: "+OPENSHIFT_ACCOUNT_DETAILS_SCHEMA_FILE);
+            Schema schemaObj = schemaFactory.newSchema(new StreamSource(xsdStream));
+
+            File xmlFile = new File(openshiftAccountDetailsFile);
+            if(!xmlFile.exists())
+            	throw new RuntimeException("validateAccountDetails() can't find xml file: "+openshiftAccountDetailsFile);
+            
+            xmlStream = new FileInputStream(xmlFile);
+            Validator v = schemaObj.newValidator();
+            v.validate(new StreamSource(xmlStream));
+        }finally {
+            if(xsdStream != null)
+                xsdStream.close();
+            if(xmlStream != null)
+                xmlStream.close();
+        }
     }
     
     private static void setProps() {
@@ -38,10 +79,17 @@ public class ShifterProvisioner {
         if(props.getProperty(OPENSHIFT_HOST) == null)
             throw new RuntimeException("must pass system property : "+OPENSHIFT_HOST);
         openshiftHost = props.getProperty(OPENSHIFT_HOST);
+        if(props.getProperty(OPENSHIFT_ACCOUNT_DETAILS_FILE_LOCATION) == null)
+            throw new RuntimeException("must pass system property : "+OPENSHIFT_ACCOUNT_DETAILS_FILE_LOCATION);
+        openshiftAccountDetailsFile = props.getProperty(OPENSHIFT_ACCOUNT_DETAILS_FILE_LOCATION);
+        if(props.getProperty(OPENSHIFT_ACCOUNT_PROVISIONING_LOG_DIR) == null)
+            throw new RuntimeException("must pass system property : "+OPENSHIFT_ACCOUNT_PROVISIONING_LOG_DIR);
+        openshiftAccountProvisioningLogDir = props.getProperty(OPENSHIFT_ACCOUNT_PROVISIONING_LOG_DIR);
 
-        StringBuilder sBuilder = new StringBuilder();
+        StringBuilder sBuilder = new StringBuilder("setProps() props = ");
         sBuilder.append("\n\topenshiftHost = "+openshiftHost);
+        sBuilder.append("\n\tpenshiftAccountDetailsFile = "+openshiftAccountDetailsFile);
+        sBuilder.append("\n\topenshiftAccountProvisioningLogDir = "+openshiftAccountProvisioningLogDir);
         log.info(sBuilder.toString());
-    
     }
 }
