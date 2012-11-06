e8e2786c8c298c5aba7cd9e78c74dd977957a0cb
Jeffrey Bride <jbride@zareason.com>
2012-11-06 22:06:01 +0100

    added sql provisioning script for mysql

--- conf/postgresql/processFlowProvision.sql
+++ conf/postgresql/processFlowProvision.sql
@@ -4,23 +4,23 @@
 -- NOTE:  both jbpm process server and human-task tables will be managed by this PostgreSQL RDBMS
 
 -- jbpm database
-drop table if exists jbpm;
-create table jbpm;
+drop database if exists jbpm;
+create database jbpm;
 alter user postgres with password 'postgres';
 create user jbpm with password 'jbpm';
 grant all privileges on database jbpm to jbpm;
 alter user jbpm with password 'jbpm';
 
 -- jbpm_bam database
-drop table if exists jbpm_bam;
-create table jbpm_bam;
+drop database if exists jbpm_bam;
+create database jbpm_bam;
 create user jbpm_bam with password 'jbpm_bam';
 grant all privileges on database jbpm_bam to jbpm_bam;
 alter user jbpm_bam with password 'jbpm_bam';
 
 -- guvnor database
-drop table if exists guvnor;
-create table guvnor;
+drop database if exists guvnor;
+create database guvnor;
 create user guvnor with password 'guvnor';
 grant all privileges on database guvnor to guvnor;
 alter user guvnor with password 'guvnor';
