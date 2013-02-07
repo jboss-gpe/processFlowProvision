df8ded335eccbeb0c2b25cb7fa84a87e724b359e
Jeffrey Bride <jbride@zareason.com>
2013-02-07 10:11:40 -0700

    updated various mysql property values

--- conf/mysql/processFlowProvision.sql
+++ conf/mysql/processFlowProvision.sql
@@ -3,10 +3,6 @@
 
 -- cat $PFP_HOME/conf/mysql/processFlowProvision.sql | mysql -u root -p mysql
 
-create database if not exists guvnor;
-grant all on guvnor.* to 'guvnor'@'localhost' identified by 'guvnor';
-grant all on guvnor.* to 'guvnor'@'%' identified by 'guvnor';
-
 create database if not exists jbpm;
 grant all on jbpm.* to 'jbpm'@'localhost' identified by 'jbpm';
 grant all on jbpm.* to 'jbpm'@'%' identified by 'jbpm';
@@ -14,9 +10,5 @@ grant all on jbpm.* to 'jbpm'@'%' identified by 'jbpm';
 create database if not exists jbpm_bam;
 grant all on jbpm_bam.* to 'jbpm_bam'@'localhost' identified by 'jbpm_bam';
 grant all on jbpm_bam.* to 'jbpm_bam'@'%' identified by 'jbpm_bam';
-
-create database if not exists bizapp;
-grant all on bizapp.* to 'bizapp'@'localhost' identified by 'bizapp';
-grant all on bizapp.* to 'bizapp'@'%' identified by 'bizapp';
 
 flush privileges;
