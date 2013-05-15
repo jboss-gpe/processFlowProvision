5ee73c8455fdc7563c4fa486ac65b945e06a80d1
Jeffrey Bride <jbride@zareason.com>
2013-05-15 06:35:59 -0600

    latest

--- pfpServices/common/src/main/java/org/jboss/processFlow/tasks/identity/PFPUserGroupCallback.java
+++ pfpServices/common/src/main/java/org/jboss/processFlow/tasks/identity/PFPUserGroupCallback.java
@@ -1,9 +1,22 @@
 package org.jboss.processFlow.tasks.identity;
 
 import java.util.List;
+import javax.enterprise.inject.Alternative;
+
+import org.jbpm.shared.services.cdi.Selectable;
 import org.kie.internal.task.api.UserGroupCallback;
 
+import org.apache.log4j.Logger;
+
+@Alternative
+@Selectable
 public class PFPUserGroupCallback implements UserGroupCallback {
+
+    private static Logger log = Logger.getLogger("PFPUserGroupCallback");
+
+    public PFPUserGroupCallback() {
+       log.info("PFPUserGroupCallback() .... constructor instantiated"); 
+    }
 
     public boolean existsUser(String userId) {
         return true;
