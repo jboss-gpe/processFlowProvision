7540fc33d21e2f67530e5ab358dea310f595a5d3
Jeffrey Bride <jbride@zareason.com>
2013-08-16 19:48:10 -0600

    deployment of both simple and git repos is now configurable through a json file

--- pfpServices/kie-services-remote/src/main/java/org/jboss/processFlow/services/remote/cdi/RESTApplicationScopedProducer.java
+++ pfpServices/kie-services-remote/src/main/java/org/jboss/processFlow/services/remote/cdi/RESTApplicationScopedProducer.java
@@ -1,9 +1,9 @@
 package org.jboss.processFlow.services.remote.cdi;
 
 
-import java.io.File;
-import java.net.URI;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -19,33 +19,15 @@ import org.jbpm.kie.services.impl.KModuleDeploymentUnit;
 import org.jbpm.kie.services.impl.VFSDeploymentUnit;
 import org.jbpm.shared.services.cdi.Selectable;
 import org.kie.internal.task.api.UserGroupCallback;
-import org.kie.commons.io.FileSystemType.Bootstrap;
 import org.kie.commons.io.IOService;
 import org.kie.commons.io.impl.IOServiceDotFileImpl;
-import org.kie.commons.io.impl.IOServiceNio2WrapperImpl;
-import org.kie.commons.io.FileSystemType;
-import org.kie.commons.java.nio.file.FileSystem;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @ApplicationScoped
 public class RESTApplicationScopedProducer {
-	
-	public static final String DEPLOYMENT_ID = "org.jboss.processFlow.vfs.deployment.id";
-    public static final String VFS_PATH = "org.jboss.processFlow.vfs.repository.folder";
-    public static final String VFS_ALIAS = "org.jboss.processFlow.vfs.repository.alias";
-    public static final String VFS_SCHEME = "org.jboss.processFlow.vfs.repository.scheme";
-    public static final String KSESSION_STRATEGY = "org.jboss.processFlow.cdi.ksession.strategy";
     
-    public static final String GIT_USER = "org.kie.services.git.user";
-    public static final String GIT_PASSWD = "org.kie.services.git.passwd";
-    public static final String GIT_REPO_URL = "org.kie.services.git.repo.url";
-    public static final String GIT_REMOTE_REPO_URL = "org.kie.services.git.remote.repo.url";
-    
-    private static String deploymentId = System.getProperty(DEPLOYMENT_ID, "please set org.jboss.processFlow.vfs.deployment.id system property");
-    private static String vfsPath = System.getProperty(VFS_PATH, null);
-    private static String remoteGitUrl = "https://github.com/guvnorngtestuser1/jbpm-console-ng-playground.git";
-    private static RuntimeStrategy ksessionStrategy = DeploymentUnit.RuntimeStrategy.PER_PROCESS_INSTANCE;
+    private static Logger log = LoggerFactory.getLogger("RESTApplicationScopedProducer");
     
     @PersistenceUnit(unitName="org.jbpm.persistence.jpa")
     EntityManagerFactory jbpmCoreEMF;
@@ -54,13 +36,9 @@ public class RESTApplicationScopedProducer {
     @Selectable
     private UserGroupCallback userGroupCallback;
     
-    private Logger log = LoggerFactory.getLogger("RESTApplicationScopedProducer");
     
     private IOService vfsIOService;
-    private String gitUser = "jboss";
-    private String gitPasswd = "bpms";
-    private String gitUrl = "git://jbpm-local";
-    private URI fsURI = null;
+    private List<DeploymentUnit> dUnits;
     
     @Produces
     public UserGroupCallback produceSelectedUserGroupCallback() {
@@ -74,38 +52,77 @@ public class RESTApplicationScopedProducer {
     
     @Produces
     @Named("deploymentUnits")
-    public List<DeploymentUnit> getDeploymentUnits() {
-    	List<DeploymentUnit> dUnits = new CopyOnWriteArrayList<DeploymentUnit>();
-    	VFSDeploymentUnit vfsUnit = createVfsDeploymentUnit();
-    	//KModuleDeploymentUnit kModule = this.createKModuleDeploymentUnit();
-    	dUnits.add(vfsUnit);
+    public List<DeploymentUnit> getDeploymentUnits() throws Exception {
+    	
+    	if(dUnits == null ) {
+    		dUnits = new CopyOnWriteArrayList<DeploymentUnit>();
+    		
+    		Map<String, Map<String, Object>> deployments = DeployUnitParser.getParsedJsonConfig();
+    		//To-Do:  parse and iterate through a JSON based file 
+    		for(Entry<String, Map<String, Object>> deployment : deployments.entrySet()) {
+    			DeploymentUnit dUnit = null;
+    			if(DeployUnitParser.GIT.equals(deployment.getKey()))
+    				dUnit = createGitDeploymentUnit(deployment.getValue());
+    			else if(DeployUnitParser.LOCAL_FILE_SYSTEM.equals(deployment.getKey()))
+    				dUnit = this.createLocalFileDeploymentUnit(deployment.getValue());
+    			else if(DeployUnitParser.KMODULE.equals(deployment.getKey()))
+    				dUnit = this.createKModuleDeploymentUnit(deployment.getValue());
+    			else
+    				throw new Exception("getDeploymentUnits() unknown deployment type: "+deployment.getKey());
+    
+    			dUnits.add(dUnit);
+    		}
+    	}
     	return dUnits;
     }
     
-    private VFSDeploymentUnit createVfsDeploymentUnit(){
-    	String vfsAlias = System.getProperty(VFS_ALIAS, null);
-    	String vfsScheme = System.getProperty(this.VFS_SCHEME, null);
-    	ksessionStrategy = RuntimeStrategy.valueOf(System.getProperty(this.KSESSION_STRATEGY, this.ksessionStrategy.toString()));
+    private VFSDeploymentUnit createLocalFileDeploymentUnit(Map<String, Object> details){
+    	RuntimeStrategy ksessionStrategy = RuntimeStrategy.valueOf((String)details.get(DeployUnitParser.KSESSION_STRATEGY));
     	StringBuilder sBuilder = new StringBuilder();
-    	sBuilder.append("start() creating deploymentUnit with \n\tdeploymentId = ");
-    	sBuilder.append(deploymentId);
-    	sBuilder.append("\n\tvfsPath = ");
-    	sBuilder.append(vfsPath);
-    	sBuilder.append("\n\tvfsAlias = ");
-    	sBuilder.append(vfsAlias);
-    	sBuilder.append("\n\tvfsScheme = ");
-    	sBuilder.append(vfsScheme);
+    	String dId = (String)details.get(DeployUnitParser.DEPLOYMENT_ID);
+    	
+    	// needs to be prefixed with "file:///"  .... will default to JGITFileSystemProvider
+    	// setting repositoryScheme to "file" does not seem to trigger use of SimpleFileSystemProvider
+    	String rFolder = "file://"+(String)details.get(DeployUnitParser.REPO_FOLDER);  
+    	
+    	String rAlias = (String)details.get(DeployUnitParser.REPO_ALIAS);
+    	sBuilder.append("createLocalFileDeploymentUnit() creating localFile deploymentUnit with \n\tdeploymentId = ");
+    	sBuilder.append(dId);
+    	sBuilder.append("\n\trepoFolder = ");
+    	sBuilder.append(rFolder);
+    	sBuilder.append("\n\trepoAlias = ");
+    	sBuilder.append(rAlias);
     	sBuilder.append("\n\tksessionStrategy = ");
     	sBuilder.append(ksessionStrategy.toString());
     	log.info(sBuilder.toString());
-    	VFSDeploymentUnit vfsUnit = new VFSDeploymentUnit(deploymentId, vfsAlias, vfsPath+deploymentId);
+    	VFSDeploymentUnit vfsUnit = new VFSDeploymentUnit(dId, rAlias, rFolder);
     	vfsUnit.setStrategy(ksessionStrategy);
-    	if(vfsScheme != null && !vfsScheme.equals(""))
-    		vfsUnit.setRepositoryScheme(vfsScheme);
     	return vfsUnit;
     }
     
-    private KModuleDeploymentUnit createKModuleDeploymentUnit() {
+    private VFSDeploymentUnit createGitDeploymentUnit(Map<String, Object> details){
+    	RuntimeStrategy ksessionStrategy = RuntimeStrategy.valueOf((String)details.get(DeployUnitParser.KSESSION_STRATEGY));
+    	StringBuilder sBuilder = new StringBuilder();
+    	String dId = (String)details.get(DeployUnitParser.DEPLOYMENT_ID);
+    	String rFolder = (String)details.get(DeployUnitParser.REPO_FOLDER);
+    	String rAlias = (String)details.get(DeployUnitParser.REPO_ALIAS);
+    	sBuilder.append("createGitDeploymentUnit() creating git deploymentUnit with \n\tdeploymentId = ");
+    	sBuilder.append(dId);
+    	sBuilder.append("\n\trepoFolder = ");
+    	sBuilder.append(rFolder);
+    	sBuilder.append("\n\trepoAlias = ");
+    	sBuilder.append(rAlias);
+    	sBuilder.append("\n\tksessionStrategy = ");
+    	sBuilder.append(ksessionStrategy.toString());
+    	log.info(sBuilder.toString());
+    	VFSDeploymentUnit vfsUnit = new VFSDeploymentUnit(dId, rAlias, rFolder);
+    	vfsUnit.setStrategy(ksessionStrategy);
+    	vfsUnit.setRepositoryScheme(DeployUnitParser.GIT);
+    	return vfsUnit;
+    }
+    
+    
+    private KModuleDeploymentUnit createKModuleDeploymentUnit(Map<String, Object> details) {
     	String ARTIFACT_ID = "test-module";
         String GROUP_ID = "org.jbpm.test";
         String VERSION = "1.0.0-SNAPSHOT";
@@ -113,57 +130,27 @@ public class RESTApplicationScopedProducer {
     	KModuleDeploymentUnit kUnit = new KModuleDeploymentUnit(GROUP_ID, ARTIFACT_ID, VERSION, "KBase-test", "ksession-test");
     	return kUnit;
     }
-
-
-    // required for ioStrategy field in:  org.jbpm.shared.services.impl.VFSFileServiceImpl    
+    
+    /* 
+     * required for ioStrategy field in:  
+       1)  org.jbpm.shared.services.impl.VFSFileServiceImpl    
+       2)  org.jboss.processFlow.services.remote.cdi.RESTApplicationStartup
+    */
     @Produces
     @Named("ioStrategy")
     public IOService getIOService() {
-        try{
-            if(vfsIOService == null) {
-            	
-            	// what's the difference between IOServiceNio2WrapperImpl and IOServiceDotFileImpl
-            	//vfsIOService = new IOServiceDotFileImpl();
-            	vfsIOService = new IOServiceNio2WrapperImpl();
-            	
-                // if vfsPath is a file system path than need to make sure that is already exists
-            	// org.kie.commons.java.nio.fs.file.SimpleFileSystemProvider will not auto-create that directory
-            	File vfsFile = new File(vfsPath + deploymentId);
-            	if(!vfsFile.exists()){
-            		log.info("getIOService() creating file system path: {}", vfsFile.getAbsolutePath());
-            		vfsFile.mkdirs();
-            	}else{
-            		log.info("getIOService() file path already created : {}", vfsFile.getAbsolutePath());
-            	}
-    
-            /*        
-            if(fsURI == null)
-                fsURI = URI.create(gitUrl);
-            
-            FileSystem fSystem = vfsIOService.getFileSystem(fsURI);
-            if(fSystem == null){
-                gitUser = System.getProperty(this.GIT_USER, gitUser);
-                gitPasswd = System.getProperty(this.GIT_PASSWD, gitPasswd);
-                gitUrl = System.getProperty(this.GIT_REPO_URL, this.gitUrl);
-                remoteGitUrl = System.getProperty(this.GIT_REMOTE_REPO_URL, remoteGitUrl);
-                StringBuilder sBuilder = new StringBuilder();
-                sBuilder.append("\n\t gitUser = "+gitUser);
-                sBuilder.append("\n\tgitUrl = "+gitUrl);
-                sBuilder.append("\n\tremoteGitUrl = "+remoteGitUrl);
-                log.info("getIOService() {}", sBuilder.toString());
-                final Map<String, Object> env = new HashMap<String, Object>();
-                env.put( "username", gitUser );
-                env.put( "password", gitPasswd);
-                env.put( "origin", remoteGitUrl);
-                vfsIOService.newFileSystem(fsURI, env, FileSystemType.Bootstrap.BOOTSTRAP_INSTANCE);
-            }else {
-                log.debug("getIOService() following FileSystem already created: {}", this.gitUrl);
-            }
-            */
-            }
-        }catch(Exception x){
-            throw new RuntimeException(x);
-        }
-        return vfsIOService;
+    	try{
+    		if(vfsIOService == null) {
+
+    			// TO-DO:  investigate the details between all of these ??
+    			vfsIOService = new IOServiceDotFileImpl();
+    			//vfsIOService = new IOServiceIndexedImpl(indexEngine, DublinCoreView.class, VersionAttributeView.class, OtherMetaView.class)
+    			//vfsIOService = vfsIOServiceClusterImpl(service, clusterServiceFactory);
+    			//vfsIOService = new IOServiceNio2WrapperImpl();
+    		}
+    		return vfsIOService;
+    	}catch(Exception x){
+    		throw new RuntimeException(x);
+    	}
     }
 }
--- pfpServices/kie-services-remote/src/main/java/org/jboss/processFlow/services/remote/cdi/RESTApplicationStartup.java
+++ pfpServices/kie-services-remote/src/main/java/org/jboss/processFlow/services/remote/cdi/RESTApplicationStartup.java
@@ -1,6 +1,11 @@
 package org.jboss.processFlow.services.remote.cdi;
 
+import java.io.File;
+import java.net.URI;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
@@ -8,16 +13,26 @@ import javax.ejb.Singleton;
 import javax.ejb.Startup;
 import javax.inject.Inject;
 import javax.inject.Named;
+
 import org.jbpm.kie.services.api.DeploymentService;
 import org.jbpm.kie.services.api.DeploymentUnit;
 import org.jbpm.kie.services.api.Vfs;
+import org.kie.commons.io.FileSystemType;
+import org.kie.commons.io.IOService;
+import org.kie.commons.java.nio.file.FileSystem;
+import org.kie.commons.java.nio.file.FileSystemAlreadyExistsException;
+
+import static org.kie.commons.io.FileSystemType.Bootstrap.BOOTSTRAP_INSTANCE;
+
+import org.kie.commons.java.nio.fs.jgit.JGitFileSystemProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Singleton
 @Startup
 public class RESTApplicationStartup {
-	private static Logger log = LoggerFactory.getLogger("RESTApplicationStartup");
+    
+    private static Logger log = LoggerFactory.getLogger("RESTApplicationStartup");
     
     @Inject
     @Vfs
@@ -27,34 +42,105 @@ public class RESTApplicationStartup {
     @Named("deploymentUnits")
     private List<DeploymentUnit> dUnits;
     
+    @Inject
+    @Named("ioStrategy")
+    private IOService ioService;
+    
+    public static enum LocalFileSystemType implements FileSystemType {
+    	LOCAL_FILE_SYSTEM;
+    	
+    	public String toString() {
+    		return "LOCAL_FILE_SYSTEM";
+    	}
+    }
     
     /*
      * Triggers org.jbpm.kie.services.impl.event.Deploy &  org.jbpm.kie.services.impl.event.UnDeploy events.
      * These events are captured by org.kie.services.remote.cdi.RuntimeManagerManager.
      */
     @PostConstruct
-    public void start() throws Exception{
+    public void start() throws Exception {
+    	this.ensureDeploymentFileSystemsExist();
     	
-    	for(DeploymentUnit dUnit : dUnits){
-    		if(deploymentService.getDeployedUnit(dUnit.getIdentifier()) == null){
-    			// there is the potential that a DeployedUnit could still be registered with :
-    			//   org.jbpm.runtime.manager.impl.AbstractRuntimeManager
-    		    deploymentService.deploy(dUnit);
-    		} else {
-    			log.error("start() uh-oh .... not going to attempt to start the following dUnit cause already registered : {}", dUnit.getIdentifier());
-    		}
-    	}
+        for(DeploymentUnit dUnit : dUnits){
+        	String dIdentifier = dUnit.getIdentifier();
+            if(deploymentService.getDeployedUnit(dIdentifier) == null){
+                // there is the potential that a DeployedUnit could still be registered with :
+                //   org.jbpm.runtime.manager.impl.AbstractRuntimeManager
+            	try {
+                    deploymentService.deploy(dUnit);
+                    log.info("start() just deployed the following dUnit : {}", dIdentifier);
+            	}catch(Throwable x){
+            		log.error("start() exception thrown when attempting to deploy the following deploymentUnit : "+dIdentifier);
+            		x.printStackTrace();
+            	}
+            } else {
+                log.error("start() uh-oh .... not going to attempt to start the following dUnit cause already registered : {}", dIdentifier);
+            }
+        }
     }
     
     @PreDestroy
     public void stop() {
-    	for (DeploymentUnit dUnit : dUnits) {
-    		if(deploymentService.getDeployedUnit(dUnit.getIdentifier()) == null)
-    			log.error("stop() uh-oh .... not going to attempt to undeploy the following dUnit which was previously not deployed : {}", dUnit.getIdentifier());
-    		else{
-    			log.info("stop() about to stop following deployment unit : {}", dUnit.getIdentifier());
-    			deploymentService.undeploy(dUnit);
-    			dUnits.remove(dUnit);
+        for (DeploymentUnit dUnit : dUnits) {
+            if(deploymentService.getDeployedUnit(dUnit.getIdentifier()) == null)
+                log.error("stop() uh-oh .... not going to attempt to undeploy the following dUnit which was previously not deployed : {}", dUnit.getIdentifier());
+            else{
+                log.info("stop() about to stop following deployment unit : {}", dUnit.getIdentifier());
+                deploymentService.undeploy(dUnit);
+                dUnits.remove(dUnit);
+            }
+        }
+    }
+    
+    private void ensureDeploymentFileSystemsExist() throws Exception {
+    	Map<String, Map<String, Object>> deployments = DeployUnitParser.getParsedJsonConfig();
+    	for(Entry<String, Map<String, Object>> deployment : deployments.entrySet()) {
+    		Map<String, Object> dHash = deployment.getValue();
+    		if(DeployUnitParser.LOCAL_FILE_SYSTEM.equals(deployment.getKey())) {
+    			String fileUri = (String)dHash.get(DeployUnitParser.REPO_FOLDER);
+    			URI fsURI = URI.create(fileUri);
+    			try{
+    				//SimpleFileSystemProvider.newFileSystem() seems to be unimplemented
+    				//final Map<String, Object> env = new HashMap<String, Object>();
+    				//ioService.newFileSystem(fsURI, env, LocalFileSystemType.LOCAL_FILE_SYSTEM);
+    				File sFile = new File(fileUri);
+    				if(!sFile.exists())
+    					sFile.mkdirs();
+    			}catch(Exception x){
+    				x.printStackTrace();
+    			}
+    		}else if(DeployUnitParser.GIT.equals(deployment.getKey())) {
+    			URI fsURI = URI.create("git://"+(String)dHash.get(DeployUnitParser.REPO_ALIAS));
+
+    			FileSystem fSystem = ioService.getFileSystem(fsURI);
+    			if(fSystem == null){
+    				String gitUser = (String)dHash.get(DeployUnitParser.GIT_USER);
+    				String gitPasswd = (String)dHash.get(DeployUnitParser.GIT_PASSWD);
+    				String localGitUrl = (String)dHash.get(DeployUnitParser.GIT_LOCAL_REPO_URL);
+    				String remoteGitUrl = (String)dHash.get(DeployUnitParser.GIT_REMOTE_REPO_URL);
+    				String gitOutDir = (String)dHash.get(DeployUnitParser.GIT_OUT_DIR);
+    				StringBuilder sBuilder = new StringBuilder();
+    				sBuilder.append("\n\tdeploymentId = "+(String)dHash.get(DeployUnitParser.DEPLOYMENT_ID));
+    				sBuilder.append("\n\tlocalGitUrl = "+localGitUrl);
+    				sBuilder.append("\n\tgitUser = "+gitUser);
+    				sBuilder.append("\n\torigin = "+remoteGitUrl);
+    				sBuilder.append("\n\tgitOutDir = "+gitOutDir);
+    				final Map<String, Object> env = new HashMap<String, Object>();
+    				env.put( JGitFileSystemProvider.USER_NAME, gitUser );
+    				env.put( JGitFileSystemProvider.PASSWORD, gitPasswd);
+    				if(remoteGitUrl == null || remoteGitUrl.equals(""))
+    					throw new Exception("ensureDeployFileSystemsExist() remoteGitUrl can not be null for deploymendId: "+(String)dHash.get(DeployUnitParser.DEPLOYMENT_ID) );
+    				env.put( JGitFileSystemProvider.GIT_DEFAULT_REMOTE_NAME, remoteGitUrl);
+    				if(gitOutDir != null && !gitOutDir.equals(""))
+    					env.put(JGitFileSystemProvider.GIT_ENV_PROP_DEST_PATH, gitOutDir);
+    				log.info("ensureDeploymentFileSystemsExist() will clone remote git repo to local as per:  {}", sBuilder.toString());
+    				ioService.newFileSystem(fsURI, env, BOOTSTRAP_INSTANCE);
+    			}else {
+    				log.warn("ensureDeploymentFileSystemsExist() following FileSystem already created: {} : scheme = {}", fsURI.toString(), fSystem.provider().getScheme());
+    			}
+    		}else{
+    			log.warn("ensureDeploymentFileSystemsExist() no need to ensure file system exists for type {}"+deployment.getKey());
     		}
     	}
     }
